84c8ffde81d88a49917a571006d445d74c2f03de
shaheerkhan00 <shaheer.khan.sk47@gmail.com>
2025-12-25 16:00:19 -0500

    caching enabled

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -5,7 +5,7 @@ from steps.model_train import model_train
 from steps.evaluation import evaluate_model
 
 
-@pipeline
+@pipeline(enable_cache=True)
 def train_pipeline(data_path:str):
     df = ingest_df(data_path)
     clean_df(df)
